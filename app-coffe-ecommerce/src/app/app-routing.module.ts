5ecaf1133a5de3485dae144bff73250e8fde4000
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-09-26 11:27:46 -0500

    error login

--- app-coffe-ecommerce/src/app/app-routing.module.ts
+++ app-coffe-ecommerce/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './Login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
